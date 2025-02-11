0dfcc661b25ff353c60d3566f00697d97fa200a2
jasonli <lijason089@gmail.com>
2025-02-11 10:56:51 -0500

    finished code

--- 3-ShellP1/dsh_cli.c
+++ 3-ShellP1/dsh_cli.c
@@ -4,52 +4,139 @@
 
 #include "dshlib.h"
 
-/*
- * Implement your main function by building a loop that prompts the
- * user for input.  Use the SH_PROMPT constant from dshlib.h and then
- * use fgets to accept user input.  Since we want fgets to also handle
- * end of file so we can run this headless for testing we need to check
- * the return code of fgets.  I have provided an example below of how
- * to do this assuming you are storing user input inside of the cmd_buff
- * variable.
- *
- *      while(1){
- *        printf("%s", SH_PROMPT);
- *        if (fgets(cmd_buff, ARG_MAX, stdin) == NULL){
- *           printf("\n");
- *           break;
- *        }
- *        //remove the trailing \n from cmd_buff
- *        cmd_buff[strcspn(cmd_buff,"\n")] = '\0';
- *
- *        //IMPLEMENT THE REST OF THE REQUIREMENTS
- *      }
- *
- *   Also, use the constants in the dshlib.h in this code.
- *      SH_CMD_MAX              maximum buffer size for user input
- *      EXIT_CMD                constant that terminates the dsh program
- *      SH_PROMPT               the shell prompt
- *      OK                      the command was parsed properly
- *      WARN_NO_CMDS            the user command was empty
- *      ERR_TOO_MANY_COMMANDS   too many pipes used
- *
- *   Expected output:
- *
- *      CMD_OK_HEADER      if the command parses properly. You will
- *                         follow this by the command details
- *
- *      CMD_WARN_NO_CMD    if the user entered a blank command
- *      CMD_ERR_PIPE_LIMIT if the user entered too many commands using
- *                         the pipe feature, e.g., cmd1 | cmd2 | ... |
- *
- *  See the provided test cases for output expectations.
- */
+// Tried but failed
+const unsigned char dragon_data[] = {
+    72, ' ', 1, '@', 4, '%', 1, '\n',
+    69, ' ', 5, '%', 1, '\n',
+    68, ' ', 5, '%', 1, '\n',
+    65, ' ', 1, '%', 6, '%', 1, '\n',
+    64, ' ', 9, '%', 5, '@', 1, '\n',
+    63, ' ', 15, '%', 5, '@', 1, '\n',
+    62, ' ', 18, '%', 2, '@', 1, '\n',
+    61, ' ', 20, '%', 3, '@', 1, '\n',
+    60, ' ', 22, '%', 4, '@', 1, '\n',
+    59, ' ', 24, '%', 5, '@', 1, '\n',
+    58, ' ', 26, '%', 6, '@', 1, '\n',
+    57, ' ', 28, '%', 7, '@', 1, '\n',
+    56, ' ', 30, '%', 8, '@', 1, '\n',
+    55, ' ', 32, '%', 9, '@', 1, '\n',
+    0 // End of compressed data
+};
+
+
+
+void print_dragon()
+{
+    const char *dragon[] = {
+        "                                                                        @%%%%",                       
+        "                                                                     %%%%%%",                         
+        "                                                                    %%%%%%",                          
+        "                                                                 % %%%%%%%           @",              
+        "                                                                %%%%%%%%%%        %%%%%%%",           
+        "                                       %%%%%%%  %%%%@         %%%%%%%%%%%%@    %%%%%%  @%%%%",        
+        "                                  %%%%%%%%%%%%%%%%%%%%%%      %%%%%%%%%%%%%%%%%%%%%%%%%%%%",          
+        "                                %%%%%%%%%%%%%%%%%%%%%%%%%%   %%%%%%%%%%%% %%%%%%%%%%%%%%%",           
+        "                               %%%%%%%%%%%%%%%%%%%%%%%%%%%%% %%%%%%%%%%%%%%%%%%%     %%%",            
+        "                             %%%%%%%%%%%%%%%%%%%%%%%%%%%%@ @%%%%%%%%%%%%%%%%%%        %%",            
+        "                            %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% %%%%%%%%%%%%%%%%%%%%%%",                
+        "                            %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%",              
+        "                            %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%@%%%%%%@",              
+        "      %%%%%%%%@           %%%%%%%%%%%%%%%%        %%%%%%%%%%%%%%%%%%%%%%%%%%      %%",                
+        "    %%%%%%%%%%%%%         %%@%%%%%%%%%%%%           %%%%%%%%%%% %%%%%%%%%%%%      @%",                
+        "  %%%%%%%%%%   %%%        %%%%%%%%%%%%%%            %%%%%%%%%%%%%%%%%%%%%%%%",                        
+        " %%%%%%%%%       %         %%%%%%%%%%%%%             %%%%%%%%%%%%@%%%%%%%%%%%",                       
+        "%%%%%%%%%@                % %%%%%%%%%%%%%            @%%%%%%%%%%%%%%%%%%%%%%%%%",                     
+        "%%%%%%%%@                 %%@%%%%%%%%%%%%            @%%%%%%%%%%%%%%%%%%%%%%%%%%%%",                  
+        "%%%%%%%@                   %%%%%%%%%%%%%%%           %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%",              
+        "%%%%%%%%%%                  %%%%%%%%%%%%%%%          %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%      %%%%",  
+        "%%%%%%%%%@                   @%%%%%%%%%%%%%%         %%%%%%%%%%%%@ %%%% %%%%%%%%%%%%%%%%%   %%%%%%%%",
+        "%%%%%%%%%%                  %%%%%%%%%%%%%%%%%        %%%%%%%%%%%%%      %%%%%%%%%%%%%%%%%% %%%%%%%%%",
+        "%%%%%%%%%@%%@                %%%%%%%%%%%%%%%%@       %%%%%%%%%%%%%%     %%%%%%%%%%%%%%%%%%%%%%%%  %%",
+        " %%%%%%%%%%                  % %%%%%%%%%%%%%%@        %%%%%%%%%%%%%%   %%%%%%%%%%%%%%%%%%%%%%%%%% %%",
+        "  %%%%%%%%%%%%  @           %%%%%%%%%%%%%%%%%%        %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%  %%%",
+        "   %%%%%%%%%%%%% %%  %  %@ %%%%%%%%%%%%%%%%%%          %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%    %%%",
+        "    %%%%%%%%%%%%%%%%%% %%%%%%%%%%%%%%%%%%%%%%           @%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%    %%%%%%%",
+        "     %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%              %%%%%%%%%%%%%%%%%%%%%%%%%%%%        %%%",
+        "      @%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%                  %%%%%%%%%%%%%%%%%%%%%%%%%",
+        "        %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%                      %%%%%%%%%%%%%%%%%%%  %%%%%%%",
+        "           %%%%%%%%%%%%%%%%%%%%%%%%%%                           %%%%%%%%%%%%%%%  @%%%%%%%%%",
+        "              %%%%%%%%%%%%%%%%%%%%           @%@%                  @%%%%%%%%%%%%%%%%%%   %%%",
+        "                  %%%%%%%%%%%%%%%        %%%%%%%%%%                    %%%%%%%%%%%%%%%    %",
+        "                %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%                      %%%%%%%%%%%%%%",
+        "                %%%%%%%%%%%%%%%%%%%%%%%%%%  %%%% %%%                      %%%%%%%%%%  %%%@",
+        "                     %%%%%%%%%%%%%%%%%%% %%%%%% %%                          %%%%%%%%%%%%%@",
+        "                                                                                 %%%%%%%@"
+    };
+
+    // Print each line
+    for (int i = 0; i < sizeof(dragon) / sizeof(dragon[0]); i++)
+    {
+        printf("%s\n", dragon[i]);
+    }
+}
+
+
+
 int main()
 {
-    char *cmd_buff;
-    int rc = 0;
-    command_list_t clist;
+    char cmd_buff[SH_CMD_MAX];  // Buffer for user input
+    command_list_t clist;       // List to hold parsed commands
+    int rc;                     // Return code from command parser
 
-    printf(M_NOT_IMPL);
-    exit(EXIT_NOT_IMPL);
+    // Main shell loop
+    while (1)
+    {
+        // Print the shell prompt
+        printf("%s", SH_PROMPT);
+
+        // Read user input
+        if (fgets(cmd_buff, SH_CMD_MAX, stdin) == NULL)
+        {
+            printf("\n");
+            break;  // Exit on EOF (Ctrl+D)
+        }
+
+        // Remove trailing newline from fgets
+        cmd_buff[strcspn(cmd_buff, "\n")] = '\0';
+
+        // Check for built-in commands
+        if (strcmp(cmd_buff, EXIT_CMD) == 0)
+        {
+            exit(OK);
+        }
+        else if (strcmp(cmd_buff, "dragon") == 0)
+        {
+            print_dragon();  // Print the compressed Drexel Dragon
+            continue;        // Continue the loop after printing
+        }
+
+        // Parse the command input
+        rc = build_cmd_list(cmd_buff, &clist);
+
+        // Handle parsing results
+        if (rc == WARN_NO_CMDS)
+        {
+            printf("%s", CMD_WARN_NO_CMD);
+        }
+        else if (rc == ERR_TOO_MANY_COMMANDS)
+        {
+            printf(CMD_ERR_PIPE_LIMIT, CMD_MAX);
+        }
+        else if (rc == OK)
+        {
+            // Print parsed commands
+            printf(CMD_OK_HEADER, clist.num);
+            for (int i = 0; i < clist.num; i++)
+            {
+                printf("<%d> %s", i + 1, clist.commands[i].exe);
+                if (strlen(clist.commands[i].args) > 0)
+                {
+                    printf(" [%s]", clist.commands[i].args);
+                }
+                printf("\n");
+            }
+        }
+    }
+
+    return OK;
 }
--- 3-ShellP1/dshlib.c
+++ 3-ShellP1/dshlib.c
@@ -5,35 +5,66 @@
 
 #include "dshlib.h"
 
-/*
- *  build_cmd_list
- *    cmd_line:     the command line from the user
- *    clist *:      pointer to clist structure to be populated
- *
- *  This function builds the command_list_t structure passed by the caller
- *  It does this by first splitting the cmd_line into commands by spltting
- *  the string based on any pipe characters '|'.  It then traverses each
- *  command.  For each command (a substring of cmd_line), it then parses
- *  that command by taking the first token as the executable name, and
- *  then the remaining tokens as the arguments.
- *
- *  NOTE your implementation should be able to handle properly removing
- *  leading and trailing spaces!
- *
- *  errors returned:
- *
- *    OK:                      No Error
- *    ERR_TOO_MANY_COMMANDS:   There is a limit of CMD_MAX (see dshlib.h)
- *                             commands.
- *    ERR_CMD_OR_ARGS_TOO_BIG: One of the commands provided by the user
- *                             was larger than allowed, either the
- *                             executable name, or the arg string.
- *
- *  Standard Library Functions You Might Want To Consider Using
- *      memset(), strcmp(), strcpy(), strtok(), strlen(), strchr()
- */
 int build_cmd_list(char *cmd_line, command_list_t *clist)
 {
-    printf(M_NOT_IMPL);
-    return EXIT_NOT_IMPL;
+    // Initialize clist
+    memset(clist, 0, sizeof(command_list_t));
+
+    // Check for empty input
+    if (strlen(cmd_line) == 0)
+    {
+        return WARN_NO_CMDS;
+    }
+
+    char *cmd_token;
+    char *saveptr;
+    int cmd_count = 0;
+
+    // Tokenize command input based on "|"
+    cmd_token = strtok_r(cmd_line, PIPE_STRING, &saveptr);
+    while (cmd_token != NULL)
+    {
+        if (cmd_count >= CMD_MAX)
+        {
+            return ERR_TOO_MANY_COMMANDS;
+        }
+
+        // Trim leading spaces
+        while (*cmd_token == SPACE_CHAR) cmd_token++;
+
+        
+        if (*cmd_token == '\0')
+        {
+            return WARN_NO_CMDS;
+        }
+
+        // Extract command name and arguments
+        char *arg_token = strchr(cmd_token, SPACE_CHAR);
+        if (arg_token != NULL)
+        {
+            *arg_token = '\0';  // Split exe and args
+            arg_token++;        // Move to args
+            while (*arg_token == SPACE_CHAR) arg_token++; // Trim leading spaces in args
+        }
+
+        // Check size constraints
+        if (strlen(cmd_token) >= EXE_MAX || (arg_token && strlen(arg_token) >= ARG_MAX))
+        {
+            return ERR_CMD_OR_ARGS_TOO_BIG;
+        }
+
+        // Store parsed command
+        strcpy(clist->commands[cmd_count].exe, cmd_token);
+        if (arg_token)
+        {
+            strcpy(clist->commands[cmd_count].args, arg_token);
+        }
+        cmd_count++;
+
+        // Get next command
+        cmd_token = strtok_r(NULL, PIPE_STRING, &saveptr);
+    }
+
+    clist->num = cmd_count;
+    return OK;
 }
