5c248eb6514b9b054c101a0fb607c4fadd7437bc
jasonli <lijason089@gmail.com>
2025-03-16 01:40:44 -0400

    Finished coding

--- 6-RShell/bats/student_tests.sh
+++ 6-RShell/bats/student_tests.sh
@@ -4,6 +4,7 @@
 # 
 # Create your unit tests suit in this file
 
+# Test `ls` command runs without errors
 @test "Example: check ls runs without errors" {
     run ./dsh <<EOF                
 ls
@@ -12,3 +13,42 @@ EOF
     # Assertions
     [ "$status" -eq 0 ]
 }
+
+# Test executing an external command with arguments
+@test "Running ls with -l flag" {
+    run ./dsh <<EOF
+ls -l
+EOF
+
+    [ "$status" -eq 0 ]
+}
+
+# Test handling `exit` command
+@test "Exit command test" {
+    run ./dsh <<EOF                
+exit
+EOF
+
+    [ "$status" -eq 0 ]
+}
+
+# Test `pwd` command
+@test "Check pwd output" {
+    run ./dsh <<EOF
+pwd
+EOF
+
+    [ "$status" -eq 0 ]
+}
+
+
+# Test running a command with multiple arguments
+@test "Run command with multiple arguments" {
+    run ./dsh <<EOF
+ls -l -a
+EOF
+
+    [ "$status" -eq 0 ]
+}
+
+
